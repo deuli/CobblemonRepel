b14f4141124cc1f00eefb709d22712052c918ada
deuli <dacostaluca@hotmail.com>
2024-08-27 16:50:54 +0200

    Initial commit

---
+++ src/main/java/deuli/cobblemonrepel/CobblemonRepel.java
@@ -0,0 +1,13 @@
+package deuli.cobblemonrepel;
+
+import net.fabricmc.api.ModInitializer;
+
+public class CobblemonRepel implements ModInitializer {
+    /**
+     * Runs the mod initializer.
+     */
+    @Override
+    public void onInitialize() {
+
+    }
+}
