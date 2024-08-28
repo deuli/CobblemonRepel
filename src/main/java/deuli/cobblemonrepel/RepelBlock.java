3363497f070e15d186674c1d77ad32337bf5fe1f
deuli <dacostaluca@hotmail.com>
2024-08-28 16:44:44 +0200

    Repel Block can now be attached to walls

--- src/main/java/deuli/cobblemonrepel/RepelBlock.java
+++ src/main/java/deuli/cobblemonrepel/RepelBlock.java
@@ -4,17 +4,23 @@ import eu.pb4.polymer.core.api.block.PolymerHeadBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.HorizontalFacingBlock;
 import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.state.StateManager;
+import net.minecraft.state.property.BooleanProperty;
+import net.minecraft.state.property.DirectionProperty;
 import net.minecraft.state.property.IntProperty;
 import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.RotationPropertyHelper;
 import org.jetbrains.annotations.Nullable;
 
 public class RepelBlock extends Block implements PolymerHeadBlock {
+    public static BooleanProperty ATTACHED = Properties.ATTACHED;
     public static final IntProperty ROTATION = Properties.ROTATION;
+    public static final DirectionProperty FACING = HorizontalFacingBlock.FACING;
 
     public RepelBlock() {
         super(Settings.create().nonOpaque().strength(2.5F, 3.5F));
@@ -23,7 +29,7 @@ public class RepelBlock extends Block implements PolymerHeadBlock {
     @Override
     protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
         super.appendProperties(builder);
-        builder.add(ROTATION);
+        builder.add(ATTACHED).add(ROTATION).add(FACING);
     }
 
     @Override
@@ -38,12 +44,19 @@ public class RepelBlock extends Block implements PolymerHeadBlock {
 
     @Override
     public BlockState getPolymerBlockState(BlockState state, ServerPlayerEntity player) {
-        return Blocks.PLAYER_HEAD.getDefaultState().with(Properties.ROTATION, state.get(Properties.ROTATION));
+        if (state.get(ATTACHED))
+            return Blocks.PLAYER_WALL_HEAD.getDefaultState().with(FACING, state.get(FACING));
+        else
+            return Blocks.PLAYER_HEAD.getDefaultState().with(Properties.ROTATION, state.get(Properties.ROTATION));
     }
 
     @Nullable
     @Override
     public BlockState getPlacementState(ItemPlacementContext ctx) {
-        return this.getDefaultState().with(ROTATION, RotationPropertyHelper.fromYaw(ctx.getPlayerYaw()));
+        Direction direction = ctx.getPlacementDirections()[0];
+        if (direction == Direction.UP || direction == Direction.DOWN)
+            return this.getDefaultState().with(ROTATION, RotationPropertyHelper.fromYaw(ctx.getPlayerYaw())).with(ATTACHED, false);
+        else
+            return this.getDefaultState().with(FACING, direction.getOpposite()).with(ATTACHED, true);
     }
 }
